e4d4fbcd561ee95bba92ca5a5a6dcc5316aa7488
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-03-11 18:20:08 +0900

    Title: 청소당번 정하기, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240311/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,11 @@
+classroom = 0
+corridor = 0
+bathroom = 0
+
+n = int(input())
+
+for i in range(1, n + 1):
+    if (i % 12 == 0):bathroom += 1
+    elif (i % 3 == 0): corridor += 1
+    elif (i % 2 == 0): classroom += 1
+print(f"{classroom} {corridor} {bathroom}")
