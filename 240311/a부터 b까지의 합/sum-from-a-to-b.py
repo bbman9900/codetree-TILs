be14f3ebb5df2b614f1127c209504929a14bf157
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-03-11 19:21:32 +0900

    Title: a부터 b까지의 합, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240311/a부터 b까지의 합/sum-from-a-to-b.py
+++ 240311/a부터 b까지의 합/sum-from-a-to-b.py
@@ -1,7 +1,2 @@
 a, b = map(int, input().split())
-pl = a + b
-mi = b - a
-if (pl % 2 == 1):
-    print(pl * (pl // 2 - 1))
-else:
-    print(pl * (pl // 2 - 2) + mi)
+print(int((a + b) * (b - a + 1) / 2))
