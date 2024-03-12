8e13bd7ecce5044289bf917790cb6d9b131a6c67
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-03-12 23:00:09 +0900

    Title: 전항의 두 배, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240312/전항의 두 배/twice-the-previous.py
+++ 240312/전항의 두 배/twice-the-previous.py
@@ -1,6 +1,14 @@
 n1, n2 = map(int, input().split())
-seq = [n1, n2]
-for i in range(2, 10):
-    seq.append(seq[i-1] + 2* seq[i-2])
-for i in seq:
-    print(i, end=" ")
+
+# seq = [n1, n2]
+# for i in range(2, 10):
+#     seq.append(seq[i-1] + 2* seq[i-2])
+# for i in seq:
+#     print(i, end=" ")
+
+print(n1, n2, end=" ")
+for _ in range(8):
+    temp = n2 + 2 * n1
+    n1 = n2
+    n2 = temp
+    print(temp, end=" ")
