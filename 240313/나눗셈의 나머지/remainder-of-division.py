e7166e62b4bcb9f0f104d5cf7b9f58c918184709
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-03-13 12:26:02 +0900

    Title: 나눗셈의 나머지, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240313/나눗셈의 나머지/remainder-of-division.py
+++ 240313/나눗셈의 나머지/remainder-of-division.py
@@ -1,12 +1,9 @@
 a, b = map(int, input().split())
-li = []
+li = [0] * b
 summ = 0
-for _ in range(b):
-    li.append(0)
-while a >= b:
+while a > 1:
     li[a % b] += 1
     a //= b
-li[a] += 1
 for i in li:
-    summ += i * i
+    summ += i ** 2
 print(summ)
