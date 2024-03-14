5c2ba290d83c07d58531974bf0d06c438928781e
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-03-14 11:33:13 +0900

    Title: 배열 놀이, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240314/배열 놀이/play-with-array.py
+++ 240314/배열 놀이/play-with-array.py
@@ -1,16 +1,15 @@
 n, q = map(int, input().split())
 li = input().split()
 for _ in range(q):
-    s = input()
-    if (s[0] == "1"):
-        print(li[int(s[2]) - 1])
-    elif (s[0] == "2"):
-        if (s[2] in li):
-            print(li.index(s[2]) + 1)
+    s = list(map(int, input().split()))
+    if (s[0] == 1):
+        print(li[s[1] - 1])
+    elif (s[0] == 2):
+        if (str(s[1]) in li):
+            print(li.index(str(s[1])) + 1)
         else:
             print(0)
-    elif (s[0] == "3"):
-        s3 = s.split()
-        for i in range(int(s3[1]) - 1, int(s3[2])):
+    elif (s[0] == 3):
+        for i in range(s[1] - 1, s[2]):
             print(li[i], end=" ")
         print("")
