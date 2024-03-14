b4c5b336b5bd61de2afe2d2e120acc1e5ae18164
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-03-14 14:09:10 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240314/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240314/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -3,8 +3,7 @@ dup = [0] * 1001
 num = []
 li = list(map(int, input().split()))
 for i in li:
-    if (dup[i] < 1):
-        dup[i] += 1
+    dup[i] += 1
 for i, v in enumerate(dup):
     if (v == 1):
         num.append(i)
