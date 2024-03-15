1dc536d073e007eb067bc1176879654c0b194b5e
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-03-15 18:27:35 +0900

    Title: 미는 횟수, Time: 161ms, Memory: 73MB, Status: Wrong Answer - CodeTree

--- 240315/미는 횟수/number-of-pushes.py
+++ 240315/미는 횟수/number-of-pushes.py
@@ -2,9 +2,7 @@ a = input()
 b = input()
 n = -1
 for i in range(len(a)):
-    if (a[i:] + a[:i] == b):
+    if (a[:i] + a[i:] == b):
         n = i
         break
-if (n == 0):
-    n = len(a)
 print(n)
