d72dcd64b6c10f785338934041fdbce0ad7085f9
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-03-19 17:37:24 +0900

    Title: 미는 횟수, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240319/미는 횟수/number-of-pushes.java
+++ 240319/미는 횟수/number-of-pushes.java
@@ -5,11 +5,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String s1 = sc.nextLine();
         String s2 = sc.nextLine();
+        int len = s1.length();
         int cnt = -1;
 
         for(int i = 0; i < s1.length() - 1; i++) {
-            if ((s1.substring(5-i) + s1.substring(0, 5-i)).equals(s2)){
-                cnt = i + 1;
+            if ((s1.substring(len-i, len) + s1.substring(0, len-i)).equals(s2)){
+                cnt = i;
                 break;
             }
         }
