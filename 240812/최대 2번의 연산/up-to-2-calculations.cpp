f1d65361a89eaa0f4fc0f72eed812cc2509c49d4
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-12 17:47:32 +0900

    Title: 최대 2번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240812/최대 2번의 연산/up-to-2-calculations.cpp
+++ 240812/최대 2번의 연산/up-to-2-calculations.cpp
@@ -6,9 +6,7 @@ int main() {
     int a;
     cin >> a;
     if (a % 2 == 0) a /= 2;
-    else a = (a + 1) / 2;
-    if (a % 2 == 0) a /= 2;
-    else a = (a + 1) / 2;
+    if (a % 2 == 1) a = (a + 1) / 2;
     cout << a;
     return 0;
 }
