4a833a97ba364c0a1daeeab5bf696b735e33728b
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-12 17:31:08 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240812/삼항연산자/ternary-operator.cpp
+++ 240812/삼항연산자/ternary-operator.cpp
@@ -5,7 +5,6 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int score;
     cin >> score;
-    if (score == 100) cout << "pass";
-    else cout << "failure";
+    cout << (score == 100 ? "pass" : "failure");
     return 0;
 }
