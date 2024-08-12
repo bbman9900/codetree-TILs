dc607e67ee251c07391c8ed357be8d27b35cd272
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-12 16:45:54 +0900

    Title: 입력받아 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240812/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int front, back;
+    char a;
+    cin >> front >> a >> back;
+    cout << front << back;
+    return 0;
+}
