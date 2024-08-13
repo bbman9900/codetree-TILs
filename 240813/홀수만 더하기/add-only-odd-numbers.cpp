0da5ff03ebb84825a41e292fe31cab38530f290d
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-13 12:49:46 +0900

    Title: 홀수만 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/홀수만 더하기/add-only-odd-numbers.cpp
+++ 240813/홀수만 더하기/add-only-odd-numbers.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n, h, cnt = 0;
+    int n, h, sum = 0;
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> h;
-        if ((h % 2 == 1) && (h % 3 == 0)) cnt++;
+        if ((h % 2 == 1) && (h % 3 == 0)) sum += h;
     }
-    cout << cnt;
+    cout << sum;
     return 0;
 }
