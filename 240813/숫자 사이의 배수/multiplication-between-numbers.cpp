97e31cd6a89227737fb18a1f76909b4f6d85b1f8
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-13 12:54:03 +0900

    Title: 숫자 사이의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/숫자 사이의 배수/multiplication-between-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, cnt = 0, sum = 0;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++) {
+        if ((i % 5 == 0) || (i % 7 == 0)) {
+            cnt++;
+            sum += i;
+        }
+    }
+    cout << fixed;
+    cout.precision(1);
+    cout << sum << " " << (float)sum / cnt;
+    return 0;
+}
