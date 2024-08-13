86407acdbdc6d0f5d8492e3180e1a31be996091a
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-13 10:37:29 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/코로나 메뉴얼/covid-manual.cpp
+++ 240813/코로나 메뉴얼/covid-manual.cpp
@@ -6,9 +6,9 @@ int main() {
     int t1, t2, t3, count = 0;
     char c1, c2, c3;
     cin >> c1 >> t1 >> c2 >> t2 >> c3 >> t3;
-    if (c1 == 'Y' || t1 >= 37) count += 1;
-    if (c2 == 'Y' || t2 >= 37) count += 1;
-    if (c3 == 'Y' || t3 >= 37) count += 1;
+    if ((c1 == 'Y') && (t1 >= 37)) count += 1;
+    if ((c2 == 'Y') && (t2 >= 37)) count += 1;
+    if ((c3 == 'Y') && (t3 >= 37)) count += 1;
     cout << ((count >= 2) ? "E" : "N");
     return 0;
 }
