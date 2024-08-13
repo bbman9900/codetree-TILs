a5b5967105ad1c2ab6225f5751617db66ffa5707
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-13 17:45:26 +0900

    Title: 약수가 세 개인 수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/약수가 세 개인 수/numbers-with-three-divisors.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int start, end, cnt = 0, divisor_cnt;
+    cin >> start >> end;
+    for (start; start <= end; start++) {
+        divisor_cnt = 0;
+        for (int i = 1; i <= start; i++) {
+            if (start % i == 0) divisor_cnt++;
+        }
+        if (divisor_cnt == 3) cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
