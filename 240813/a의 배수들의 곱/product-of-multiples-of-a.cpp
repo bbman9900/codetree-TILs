ed9b2571d7d91c1c1b268a67bdbc5e0f1b96d3f9
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-13 13:31:26 +0900

    Title: a의 배수들의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/a의 배수들의 곱/product-of-multiples-of-a.cpp
+++ 240813/a의 배수들의 곱/product-of-multiples-of-a.cpp
@@ -5,7 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, prod = 1;
     cin >> a >> b;
-    for (int i = 1; i < b; i++) if (i % a == 0) prod *= i;
+    for (int i = 1; i <= b; i++) if (i % a == 0) prod *= i;
     cout << prod;
     return 0;
 }
