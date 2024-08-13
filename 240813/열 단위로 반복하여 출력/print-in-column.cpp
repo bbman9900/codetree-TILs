75dacf11181c93ab2275e03a658359b3b8626d02
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-13 15:36:29 +0900

    Title: 열 단위로 반복하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/열 단위로 반복하여 출력/print-in-column.cpp
+++ 240813/열 단위로 반복하여 출력/print-in-column.cpp
@@ -6,7 +6,7 @@ int main() {
     int n;
     cin >> n;
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) cout << j;
+        for (int j = 1; j <= n; j++) cout << i;
         cout << endl;
     }
     return 0;
