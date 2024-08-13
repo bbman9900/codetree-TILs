c57de8ef7d8ef08107a8f6ff11231e39ac18873f
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-13 10:13:45 +0900

    Title: 숫자의 계절은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/숫자의 계절은/season-of-num.cpp
+++ 240813/숫자의 계절은/season-of-num.cpp
@@ -5,13 +5,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int m;
     cin >> m;
-    if (m == 12) {
-        cout << "Winter";
-        return 0;
-    } else if (m >= 9) cout << "Fall";
-    else if (m >= 6) cout << "Summer";
-    else if (m >= 3) cout << "Spring";
-    else cout << "Winter";
+    if(m >= 12 || m <= 2) cout << "Winter";
+	else if(m <= 5) cout << "Spring";
+	else if(m <= 8) cout << "Summer";
+	else cout << "Fall";
 
     return 0;
 }
