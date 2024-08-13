37bd8db5bd2326f77c57af9e75f07cab31598a74
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-13 10:21:57 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/두 사람/two-person.cpp
+++ 240813/두 사람/two-person.cpp
@@ -7,5 +7,6 @@ int main() {
     char as, bs;
     cin >> aa >> as >> ba >> bs;
     if (((aa >= 19) && (as == 'M')) || ((ba >= 19) && (bs == 'M'))) cout << 1;
+    else cout << 0;
     return 0;
 }
