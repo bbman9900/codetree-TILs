1259bc5044d1cfb5e1b9b298672f44f204361784
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-16 12:42:32 +0900

    Title: 지그재그로 숫자 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240816/지그재그로 숫자 채우기/zigzag-numbering.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m, num = 0;
+    cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0; i < m; i++) {
+        if (i % 2 == 1) for (int j = n - 1; j >= 0; j--) arr[j][i] = num++;
+        else for (int j = 0; j < n; j++) arr[j][i] = num++;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
