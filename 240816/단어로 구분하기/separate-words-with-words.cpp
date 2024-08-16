8dd800fcfd5debe7d85fe7e6eed2be515b809f5e
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-16 16:10:07 +0900

    Title: 단어로 구분하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240816/단어로 구분하기/separate-words-with-words.cpp
+++ 240816/단어로 구분하기/separate-words-with-words.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     string str[10];
-    for (int i = 0; i < 10; i++) cin >> str[i];
-    for (int i = 0; i < 10; i++) cout << str[i] << endl;
+    for (int i = 0; i < 10; i++) {
+        cin >> str[i];
+        cout << str[i] << endl;
+    }
     return 0;
 }
