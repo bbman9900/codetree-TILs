31714f0147bfe0273c886b6be4d1f5d75a79f2f9
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-16 09:23:11 +0900

    Title: 특정 숫자 도달하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240816/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10], sum = 0, cnt = 0;
+    bool over250 = false;
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+        if (arr[i] >= 250) break;
+        sum += arr[i];
+        cnt++;
+    }
+    cout << sum << " " << (float)sum / cnt;
+    return 0;
+}
