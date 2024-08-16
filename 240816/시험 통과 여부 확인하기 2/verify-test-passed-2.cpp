33cb6efa6f2b448fe0d69e513a1ab6151882fe27
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-16 09:43:37 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240816/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, arr[4], cnt = 0, sum;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        sum = 0;
+        for (int j = 0; j < n; j++) {
+            cin >> arr[j];
+            sum += arr[j];
+        }
+        if (sum / 4 >= 60) {
+            cnt++;
+            cout << "pass\n";
+        } else cout << "fail\n";
+    }
+    cout << cnt;
+    return 0;
+}
