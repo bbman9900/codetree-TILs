a3e2761934f832f97a629ce0a6757693e055376c
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-16 11:26:18 +0900

    Title: 주사위 놀이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240816/주사위 놀이/play-with-dice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, arr[7] = {};
+    for (int i = 0; i < 10; i++) {
+        cin >> a;
+        arr[a]++;
+    }
+    for (int i = 1; i < 7; i++) {
+        cout << i << " - " << arr[i] << endl;
+    }
+    return 0;
+}
