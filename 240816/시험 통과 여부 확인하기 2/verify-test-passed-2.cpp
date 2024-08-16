3f73413f87d4637aa295253c1f37dcca84582142
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-16 09:46:25 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240816/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
+++ 240816/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         sum = 0;
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < 4; j++) {
             cin >> arr[j];
             sum += arr[j];
         }
