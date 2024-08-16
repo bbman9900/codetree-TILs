5a2d8738ec8cfccf46eba8f9e91d1565242192bf
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-16 16:17:14 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240816/특정 문자로 끝나는 문자열/string-ending-with-specific-character.cpp
+++ 240816/특정 문자로 끝나는 문자열/string-ending-with-specific-character.cpp
@@ -6,12 +6,17 @@ int main() {
     // 여기에 코드를 작성해주세요.
     string str[10];
     char c;
+    bool is = false;
     for (int i = 0; i < 10; i++) {
         cin >> str[i];
     }
     cin >> c;
     for (int i = 0; i < 10; i++) {
-        if (str[i][str[i].length() - 1] == c) cout << str[i] << endl;
+        if (str[i][str[i].length() - 1] == c) {
+            cout << str[i] << endl;
+            is = true;
+        } 
     }
+    if (!is) cout << "None";
     return 0;
 }
