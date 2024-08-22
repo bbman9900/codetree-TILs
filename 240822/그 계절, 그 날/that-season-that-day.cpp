20f0e7b37bb0703a27c94139d02ac575a5983fd2
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-22 15:45:17 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240822/그 계절, 그 날/that-season-that-day.cpp
+++ 240822/그 계절, 그 날/that-season-that-day.cpp
@@ -6,19 +6,19 @@ bool MoonCheck(int y) {
     else return true;
 }
 
-string Season(int y, int m, int d) {
+void Season(int y, int m, int d) {
     if (m == 12 || m <= 2) {
-        if (m == 2 && d == 29) return (MoonCheck(y) ? "Winter" : "-1");
-        return "Winter";
-    } else if (m >= 9) return "Fall";
-    else if (m >= 6) return "Summer";
-    else return "Spring";
+        if (m == 2 && d == 29) cout << (MoonCheck(y) ? "Winter" : "-1");
+        else cout << "Winter";
+    } else if (m >= 9) cout << "Fall";
+    else if (m >= 6) cout << "Summer";
+    else cout << "Spring";
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int y, m, d;
     cin >> y >> m >> d;
-    cout << Season(y, m, d);
+    Season(y, m, d);
     return 0;
 }
