9bb082b3aab5695c2f0c70c361864a5f79fe3ef4
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-22 15:49:17 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240822/그 계절, 그 날/that-season-that-day.cpp
+++ 240822/그 계절, 그 날/that-season-that-day.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool MoonCheck(int y) {
-    if (y % 4 != 0 || (y % 100 == 0 && y % 400 != 0)) return false;
+    if (y % 4 != 0 && y % 400 != 0) return false;
     else return true;
 }
 
