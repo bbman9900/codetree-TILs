f07dcb7d151c86d939701ef3c6d11c178f103690
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-22 11:45:06 +0900

    Title: 붙여서 합하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240822/붙여서 합하기/add-and-add.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a, b, ab, ba;
+    cin >> a >> b;
+    ab = a + b;
+    ba = b + a;
+    cout << stoi(ab) + stoi(ba);
+    return 0;
+}
