c922ffca138919c9989e25e5fa06bff312c8e3b7
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-22 11:11:47 +0900

    Title: 아스키코드표 맞추기, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240822/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a1, a2, a3, a4, a5;
+    cin >> a1 >> a2 >> a3 >> a4 >> a5;
+    cout << (char)a1 << " " << (char)a2 <<  " " << (char)a3 <<  " " << (char)a4 << " " << (char)a5;
+    return 0;
+}
