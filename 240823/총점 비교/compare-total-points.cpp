bb7b0e324ba349f17146fb604acd31e33511e121
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-23 16:45:21 +0900

    Title: 총점 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240823/총점 비교/compare-total-points.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Human {
+    public:
+        string s; int kr, en, math;
+        Human(string s, int kr, int en, int math) {
+            this->s = s;
+            this->kr = kr;
+            this->en = en;
+            this-> math = math;
+        }
+
+        Human() {}
+};
+
+bool cmp(const Human &a, const Human &b) {
+    return a.kr + a.en + a.math < b.kr + b.en + b.math;
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, kr, en, math;
+    string s;
+    cin >> n;
+    Human human[n];
+    for (int i = 0; i < n; i++) {
+        cin >> s >> kr >> en >> math;
+        human[i] = Human(s, kr, en, math);
+    }
+    sort(human, human + n, cmp);
+    for (int i = 0; i < n; i++) cout << human[i].s << " " << human[i].kr << " " << human[i].en << " " << human[i].math << endl;
+    return 0;
+}
