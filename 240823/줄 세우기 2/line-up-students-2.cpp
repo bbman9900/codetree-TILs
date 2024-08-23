82c6baf531fa242a789a87dc50c676d44632ddad
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-23 17:27:31 +0900

    Title: 줄 세우기 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240823/줄 세우기 2/line-up-students-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Student {
+    public:
+        int h, w, num;
+        Student(int h, int w, int num) {
+            this->h = h;
+            this->w = w;
+            this->num = num;
+        }
+
+        Student() {}
+};
+
+bool cmp(const Student &a, const Student &b) {
+    if (a.h < b.h || (a.h == b.h && a.w > b.w)) return true;
+    else return false;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, h, w, num;
+    cin >> n;
+    Student student[n];
+    for (int i = 0; i < n; i++) {
+        cin >> h >> w;
+        student[i] = Student(h, w, i + 1);
+    }
+    sort(student, student + n, cmp);
+    for (int i = 0; i < n; i++) cout << student[i].h << " " << student[i].w << " " << student[i].num << endl;
+    return 0;
+}
