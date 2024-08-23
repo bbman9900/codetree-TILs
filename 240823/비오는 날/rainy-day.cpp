bfd0be36c0aafaebb0144f8a3d74f9bf33476287
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-23 15:22:04 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240823/비오는 날/rainy-day.cpp
+++ 240823/비오는 날/rainy-day.cpp
@@ -23,16 +23,21 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> date >> days >> weather;
         yy = stoi(date.substr(0, 4));
+        // cout << "year : " << yy << " ";
         mm = stoi(date.substr(5, 2));
+        // cout << "month : " << mm << " ";
         dd = stoi(date.substr(8, 2));
+        // cout << "day : " << dd << endl;
         if (weather == "Rain" && (yy <= y)) {
             if (yy == y) {
                 if (mm > m) continue;
-                else if (dd >= d) continue;
+                else if (mm >= m) {
+                    if (dd >= d) continue;
+                }
             }
             y = yy; m = mm; d = dd;
             rainyDay = RainyDay(date, days, weather);
-        } 
+        }
     }
     cout << rainyDay.date << " " << rainyDay.days << " " << rainyDay.weather; 
 
