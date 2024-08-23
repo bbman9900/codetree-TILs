06d48714da5b8b80f49a4310a5fb3a1916f8f34e
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-23 12:31:03 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240823/단어 정렬/sorting-words.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    string s[n];
+    for (int i = 0; i < n; i++) cin >> s[i];
+    sort(s, s + n);
+    for (int i = 0; i < n; i++) cout << s[i] << endl;
+    return 0;
+}
