4b852f9ae9faf16a95be762e15d40982a651d5b2
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-23 09:45:29 +0900

    Title: 숫자 차례로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240823/숫자 차례로 출력하기/to-output-numerically.cpp
+++ 240823/숫자 차례로 출력하기/to-output-numerically.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int up(int now, int n) {
-    cout << now << " ";
-    if (now < n) up(now + 1, n);
-    return 0;
+void up(int n) {
+    if (n == 0) return;
+    up(n - 1);
+    cout << n << " ";
 }
 
-int down(int n) {
+void down(int n) {
+    if (n == 0) return;
     cout << n << " ";
-    if (n > 1) down(n - 1);
-    return 0;
+    down(n - 1);
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    up(1, n);
+
+    up(n);
     cout << endl;
     down(n);
     return 0;
