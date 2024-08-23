2cdf104368ae6271cf07493b02c529941cc0cfc5
bbman9900 <66197184+bbman9900@users.noreply.github.com>
2024-08-23 22:21:10 +0900

    Title: 2진수로 변환하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240823/2진수로 변환하기/convert-to-binary.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string Binary(int n) {
+    if (n / 2 < 2) return to_string(n / 2) + to_string(n % 2);
+    return Binary(n / 2) + to_string(n % 2);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    cout << Binary(n);
+    return 0;
+}
